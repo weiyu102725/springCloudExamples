1656e0a7fe80bd9d6c362d19390211f679ff9677
nicky <mazq@minstone.com.cn>
2020-07-27 13:18:22 +0800

    SpringCloud eureka例子工程

---
+++ springcloud-eureka/springcloud-eureka-client/src/test/java/com/example/spirngcloud/client/SpringcloudEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngcloud.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
