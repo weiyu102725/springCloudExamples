937e4570526ba908f6fd083b234f9730cb5573f5
nicky <mazq@minstone.com.cn>
2020-07-28 16:42:26 +0800

    spring cloud  feign

---
+++ springcloud-feign/src/main/java/com/example/springcloud/feign/SpringcloudFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudFeignApplication.class, args);
+    }
+
+}
