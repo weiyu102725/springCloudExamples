55ea405436de4cb4ed7b21050f20330335ae2697
nicky <mazq@minstone.com.cn>
2020-08-01 18:25:58 +0800

    Spring cloud hystrix example

--- springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/controller/RestController.java
+++ springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/controller/RestController.java
@@ -1,7 +1,9 @@
 package com.example.springcloud.hystrix.controller;
 
 import com.example.springcloud.hystrix.bean.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.ribbon.proxy.annotation.Hystrix;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +21,31 @@ import org.springframework.web.client.RestTemplate;
  * </pre>
  */
 @org.springframework.web.bind.annotation.RestController
+@Slf4j
 public class RestController {
 
     @Autowired
     RestTemplate restTemplate;
 
+    /**
+     * @HystrixCommand注解指定异常时调用的方法
+     * @Author mazq
+     * @Date 2020/08/01 18:17
+     * @Param [username]
+     * @return
+     */
     @GetMapping("/findUser/{username}")
+    @HystrixCommand(fallbackMethod = "userApiFallback")
     public User index(@PathVariable("username")String username){
         return restTemplate.getForObject("http://EUREKA-SERVICE-PROVIDER/api/users/"+username,User.class);
+    }
+
+    public User userApiFallback(String username) {
+        log.info("fallback方法，接收的参数：username = {}",username);
+        User user = new User();
+        user.setName("defaultUser");
+        user.setBlog("https://smilenicky.blog.csdn.net");
+        return user;
     }
 
 }
