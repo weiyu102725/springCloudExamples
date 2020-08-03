9b69e4ee53e15373692331a21465b211c0f4df05
nicky <mazq@minstone.com.cn>
2020-08-03 15:49:54 +0800

    Spring cloud hystrix example

--- springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/SpringcloudHystrixApplication.java
+++ springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/SpringcloudHystrixApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
+//@EnableHystrix
 @EnableEurekaClient
 public class SpringcloudHystrixApplication {
 
--- springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/component/FeignHystrixClient.java
+++ springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/component/FeignHystrixClient.java
@@ -2,7 +2,6 @@ package com.example.springcloud.hystrix.component;
 
 import com.example.springcloud.hystrix.bean.User;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
