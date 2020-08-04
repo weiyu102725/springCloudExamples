d094caa36dcb3460e5c767a9c5cb966e9d3849f4
nicky <mazq@minstone.com.cn>
2020-08-04 17:13:09 +0800

    Spring cloud hystrix example

--- springcloud-eureka/springcloud-service-provider/src/main/java/com/example/springcloud/provider/SpringcloudServiceProviderApplication.java
+++ springcloud-eureka/springcloud-service-provider/src/main/java/com/example/springcloud/provider/SpringcloudServiceProviderApplication.java
@@ -2,10 +2,13 @@ package com.example.springcloud.provider;
 
 import com.example.springcloud.provider.bean.User;
 import com.example.springcloud.provider.service.UserService;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 public class SpringcloudServiceProviderApplication {
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     @Autowired
     UserService userService;
@@ -26,8 +39,8 @@ public class SpringcloudServiceProviderApplication {
     @GetMapping({"/api/users/{username}"})
     @ResponseBody
     public User findUser(@PathVariable("username")String username) throws InterruptedException{
-        //User user=  userService.findUser(username);
-        User user = new User("nicky","http://smilenicky.blog.csdn.net");
+        User user=  userService.findUser(username);
+        //User user = new User("nicky","http://smilenicky.blog.csdn.net");
         return user;
     }
 }
--- springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/SpringcloudHystrixApplication.java
+++ springcloud-hystrix/src/main/java/com/example/springcloud/hystrix/SpringcloudHystrixApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 //@EnableHystrix
 @EnableEurekaClient
+@EnableTurbine
 public class SpringcloudHystrixApplication {
 
     public static void main(String[] args) {
