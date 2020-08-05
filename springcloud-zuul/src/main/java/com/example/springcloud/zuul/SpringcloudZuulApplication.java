af6e2ebc8e282b953576a88700de478f1cf3e774
nicky <mazq@minstone.com.cn>
2020-08-05 15:01:39 +0800

    spring cloud zuul example

---
+++ springcloud-zuul/src/main/java/com/example/springcloud/zuul/SpringcloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudZuulApplication.class, args);
+    }
+
+}
